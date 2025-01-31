9cbd6699dd3c97009e3e5c1af675cf6b85c2d510
Abdin71 <abdin.huss@gmail.com>
2025-01-31 12:04:01 +0100

    Updated Readme file

--- Projects/Books/Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ Projects/Books/Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a37ac7df0c473b31421639f2cccc921dbf98240e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf78a9edef5f34d1217f6de389c64ef61ddbdab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
